2f1865bff05d19d0925e1af96e43392c044f8d1c
gourav-cardekho <gourav.kumar1@gaadi.com>
2022-04-05 16:03:41 +0530

    Inital Project setup

---
+++ ChatingLibExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatingLibExample
+//
+//  Created by Mac Pro on 05/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ChattingLib/ChattingLib.h
@@ -0,0 +1,18 @@
+//
+//  ChattingLib.h
+//  ChattingLib
+//
+//  Created by Mac Pro on 05/04/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChattingLib.
+FOUNDATION_EXPORT double ChattingLibVersionNumber;
+
+//! Project version string for ChattingLib.
+FOUNDATION_EXPORT const unsigned char ChattingLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChattingLib/PublicHeader.h>
+
+
