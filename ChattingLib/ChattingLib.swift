be4306ced2a262cc5b3f79e376bd9fafe9035101
gourav-cardekho <gourav.kumar1@gaadi.com>
2022-04-05 16:20:40 +0530

    Added test cases

---
+++ ChattingLib/ChattingLib.swift
@@ -0,0 +1,22 @@
+//
+//  ChattingLib.swift
+//  ChattingLib
+//
+//  Created by Mac Pro on 05/04/22.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
